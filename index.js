31b69bb2c9282eac0a7767a3d0eb87a685334950
dhruv00712 <144476091+dhruv00712@users.noreply.github.com>
2025-05-22 12:04:05 +0530

    index.js

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
-// const express = require('express');
-// const app =express();
-
-// app.get('/',(req,res)=> {
-//     res.send("Hello World");
-
-// });
-
-// app.listen(9000,()=> {
-//     console.log('server running on port 9000');
-// });
-
 
 
 const express = require('express');
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.send("Hello World");
+    res.send("My First Application");
 });
 
 app.listen(9000, ()=>{
