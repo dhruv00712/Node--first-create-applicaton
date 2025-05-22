ddaff2784df3645877f4a27a83e9baee6e77a69f
dhruv00712 <144476091+dhruv00712@users.noreply.github.com>
2025-05-22 12:01:52 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// const express = require('express');
+// const app =express();
+
+// app.get('/',(req,res)=> {
+//     res.send("Hello World");
+
+// });
+
+// app.listen(9000,()=> {
+//     console.log('server running on port 9000');
+// });
+
+
+
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send("Hello World");
+});
+
+app.listen(9000, ()=>{
+    console.log('server running on 9000');
+});
+
